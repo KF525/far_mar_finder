706476f8a8e06b782ca497fd564493e32eece46a
Kate Fulton <katesuzannefulton@gmail.com>
2014-09-15 15:15:20 -0700

    Hard code load of csv file

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -1,4 +1,17 @@
 module FarMar
   class Market
+    attr_reader :id, :name, :address, :city, :country, :state, :zip
+
+    def initialize
+      @market_data = CSV.read("/Users/katefulton/documents/ADA/week03/projects/far_mar_finder/support/markets.csv/far_mar_finder/support/markets.csv", "r")
+    end
+
+    def self.all
+      
+      puts @market_data.count
+    end
+
+    def self.find(id)
+    end
   end
 end
