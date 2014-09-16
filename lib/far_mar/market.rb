ada61df6851f91b20fbc4fb5a68323d39c636950
Kate Fulton <katesuzannefulton@gmail.com>
2014-09-16 12:36:22 -0700

    Update/clean up self.all method, create self.find method.

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -1,24 +1,22 @@
 module FarMar
   class Market
-    attr_reader :id, :name, :address, :city, :country, :state, :zip
+    attr_accessor :id, :name, :address, :city, :country, :state, :zip
 
     def initialize(market_array)
       @id = market_array[0]
-      # @name = market_array[1]
-      # @address = market_array[2]
-      # @city = market_array[3]
-      # @country = market_array[4]
-      # @state = market_array[5]
-      # @zip = market_array[6]
+      @name = market_array[1]
+      @address = market_array[2]
+      @city = market_array[3]
+      @country = market_array[4]
+      @state = market_array[5]
+      @zip = market_array[6]
     end
 
     def self.all
         @market_data = CSV.read("support/markets.csv", "r")
         @market_data.collect do |row|
-          market = FarMar::Market.new(row)
-        #  market.inspect
+          FarMar::Market.new(row)
         end
-
     end
 
     def count
@@ -26,9 +24,10 @@ module FarMar
     end
 
     def self.find(id)
-      self.all.find{|num| num[0].to_i == id.to_i}
-
+      self.all.find {|num| num[0].to_i == id}
     end
-
   end
 end
+
+
+#rspec spec/lib/far_mar/market_spec.rb
