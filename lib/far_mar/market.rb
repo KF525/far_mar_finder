9ac9a4f980ce75fda0a133a7e788a5747a8120cb
Kate Fulton <katesuzannefulton@gmail.com>
2014-09-15 17:03:38 -0700

    Add instance variables, adjust self.all to create new market objects.

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -2,17 +2,34 @@ module FarMar
   class Market
     attr_reader :id, :name, :address, :city, :country, :state, :zip
 
-    #This is a class/factory method, the factory is creating the instances
+    def initialize#(market_array)
+      @id = market_array[0]
+      @name = market_array[1]
+      @address = market_array[2]
+      @city = market_array[3]
+      @country = market_array[4]
+      @state = market_array[5]
+      @zip = market_array[6]
+    end
+
     def self.all
-      CSV.read("support/markets.csv", "r")
-    
+        @market_data = CSV.read("support/markets.csv", "r")
+        @market_data.each do |array|
+          market = FarMar::Market.new(array)
+          market.inspect
+      end
     end
 
     def count
       self.all.count
     end
 
-    def find(id)
+    def self.find(id)
+      @market_data.each do |row|
+        #if row[0] == id
+          print row
+        #end
+      end
     end
   end
 end
