f43e98448ca7bd9e2b657f24d671bab7642199a8
Bonnie <kbonnie@gmail.com>
2014-09-16 11:23:53 -0700

    tues morning

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -2,22 +2,23 @@ module FarMar
   class Market
     attr_reader :id, :name, :address, :city, :country, :state, :zip
 
-    def initialize#(market_array)
+    def initialize(market_array)
       @id = market_array[0]
-      @name = market_array[1]
-      @address = market_array[2]
-      @city = market_array[3]
-      @country = market_array[4]
-      @state = market_array[5]
-      @zip = market_array[6]
+      # @name = market_array[1]
+      # @address = market_array[2]
+      # @city = market_array[3]
+      # @country = market_array[4]
+      # @state = market_array[5]
+      # @zip = market_array[6]
     end
 
     def self.all
         @market_data = CSV.read("support/markets.csv", "r")
-        @market_data.each do |array|
-          market = FarMar::Market.new(array)
-          market.inspect
-      end
+        @market_data.collect do |row|
+          market = FarMar::Market.new(row)
+        #  market.inspect
+        end
+
     end
 
     def count
@@ -25,11 +26,9 @@ module FarMar
     end
 
     def self.find(id)
-      @market_data.each do |row|
-        #if row[0] == id
-          print row
-        #end
-      end
+      self.all.find{|num| num[0].to_i == id.to_i}
+
     end
+
   end
 end
